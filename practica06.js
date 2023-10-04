415635ba5c49f634e1fefc72ee474b1adb7b1b59
PeppeGC17 <pepegarciacalderon7@gmail.com>
2023-10-04 11:48:53 -0600

    practica06

---
+++ practica06.js
@@ -0,0 +1,8 @@
+const formulario = document.getElementById("formulario")
+formulario.addEventListener("click", event =>{
+    event.preventDefault7()
+    let u=documet.getElementById("user")
+    let p=documet.getElementById("password")
+
+alert(u+p)
+})
